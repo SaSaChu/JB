6028663568c54958b49a737bb27cb16b19acdce1
SaSaChu <teresa7397@gmail.com>
2021-03-10 20:55:06 +0800

    開版完成

--- js/public.js
+++ js/public.js
@@ -1,4 +1,17 @@
 
  
 $(function() {
+
+  $('.lMenu').click(function() {
+    $('.lMenu').toggleClass('lMenu-show');
+    $('.block-l').toggleClass('block-l-show');
+    $('.block-r').toggleClass('block-r-show');
+  });
+
+  $('.block-l-menuBox').each (function () {
+    var $that = $(this);
+    $that.find ('.m-menu-t').click (function () {
+      $(this).toggleClass ('s');
+    });
+  });
 });
