f94fd6b29d0d3f18ac5d7b8a7a3ba56f81fd3cdb
TeresaChu <tsgo5888@gmail.com>
2021-03-14 18:45:43 +0800

    剩一些

--- js/public.js
+++ js/public.js
@@ -14,4 +14,26 @@ $(function() {
       $(this).toggleClass ('s');
     });
   });
+
+  // 光箱
+  $('.showModel').click(function() {
+    $('#myModel').addClass('showModel')
+
+    $('#myModel .model_boxs.box_' + $(this).data('id')).addClass('show').siblings().removeClass('show')
+  })
+
+  $('.btn_close, .t-close').click(function() {
+    $('#myModel').removeClass('showModel')
+  })
+
+  // 換頁籤
+  $('.link_order_info').click(function() {
+    $('.link_order_info').removeClass ('loi_s');
+    $(this).addClass ('loi_s');
+
+    $('.order_info').removeClass('order_info_show');
+    $('.order_info').eq($(this).index()).addClass('order_info_show');
+   });
+
+  $('.link_order_info').eq (0).click();
 });
